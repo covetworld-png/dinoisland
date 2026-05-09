3af3de948ac753f7abd07ef9d5ac138080609a1a
covetworld-png <covetworld@gmail.com>
2026-05-09 12:47:38 +0800

    chore(guild-bot-lite): 修正多服mock数据公会名格式

--- src/tools/guild-recruitment-bot/js/app_lite.js
+++ src/tools/guild-recruitment-bot/js/app_lite.js
@@ -93,7 +93,7 @@ let players = [
   { id: '13229549', nickname: 'EmSee/EmSee', server: 'Q/K', regDate: '2026-05-09', tag: 'new', guildStatus: 'no_guild', guildName: null, onlineStatus: 'online', onlineSince: '08:00:00', offlineTime: null },
   { id: '13227200', nickname: 'TaoLaCho/TaoLaCho', server: 'Q/K', regDate: '2026-04-01', tag: 'returning', guildStatus: 'no_guild', guildName: null, onlineStatus: 'online', onlineSince: '04:00:00', offlineTime: null },
   { id: '13225575', nickname: 'cuccucthanbi/chacuatao', server: 'Q/K', regDate: '2026-03-07', tag: 'returning', guildStatus: 'no_guild', guildName: null, onlineStatus: 'online', onlineSince: '10:00:00', offlineTime: null },
-  { id: '13229999', nickname: 'PlayerA/PlayerB', server: 'Q/K', regDate: '2026-05-09', tag: 'new', guildStatus: 'has_guild/no_guild', guildName: 'TOP.Legend/', onlineStatus: 'online', onlineSince: '11:30:00', offlineTime: null },
+  { id: '13229999', nickname: 'PlayerA/PlayerB', server: 'Q/K', regDate: '2026-05-09', tag: 'new', guildStatus: 'has_guild/no_guild', guildName: 'TOP.Legend', onlineStatus: 'online', onlineSince: '11:30:00', offlineTime: null },
 ];
 
 let currentFilter = 'all';
